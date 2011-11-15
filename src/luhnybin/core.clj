42070bc1acff260a668f298cb6f6d615534650c7
Chris Gray <chrismgray@gmail.com>
2011-11-15 15:12:20 -0700

    Put in type hints to speed up execution

--- src/luhnybin/core.clj
+++ src/luhnybin/core.clj
@@ -2,7 +2,7 @@
   (:gen-class)
   (:require [clojure.string :as string]))
 
-(defn luhn-check
+(defn ^Boolean luhn-check
   "Takes a sequence of digits and determines whether they pass the Luhn test.
    The sequence must be in order from right to left."
   [digits]
@@ -13,20 +13,20 @@
        (#(mod % 10))
        (= 0)))
 
-(defn digit? [char]
+(defn ^Boolean digit? [^Character char]
   (contains? (set "0123456789") char))
 
-(defn separator? [char]
+(defn ^Boolean separator? [^Character char]
   (contains? (set " -") char))
 
-(defn digit->int [char]
+(defn ^Integer digit->int [^Character char]
   (Integer/parseInt (str char)))
 
-(defn replace-digits
+(defn ^String replace-digits
   "Takes a (reversed) string, and a pair with first and last indices
    of a number that passes the Luhn test.  Replaces all the digits in
    the string between these two indices with Xs."
-  [s [first-idx last-idx]]
+  [^String s [^Integer first-idx ^Integer last-idx]]
   (str (apply str (vec (take first-idx s))) ; take returns a lazy sequence, so force it with vec
        (apply str
               (reduce (fn [s letter]
@@ -42,7 +42,7 @@
    (possibly separated by spaces or dashes) pass the Luhn check, return a vector
    of the index in the string and the last index in the string which contains a
    digit that passes the Luhn check.  Otherwise, return nil."
-  [s idx]
+  [^String s ^Integer idx]
   (let [digits
         (->> s
              (#(subs % idx))
@@ -59,9 +59,9 @@
                    (when (and (>= num-digits num) (luhn-check (map first (take num digits))))
                      [idx (second (last (take num digits)))])) '(16 15 14))))))
 
-(defn handle-input-line
+(defn ^String handle-input-line
   "Takes a line of input and replaces the possible credit-card numbers with Xs."
-  [line]
+  [^String line]
   (let [line (apply str (reverse line))
         replace-between-indexes (remove nil? (map handle-string-at-index (repeat line) (range (count line))))]
     (reduce replace-digits line replace-between-indexes)))
